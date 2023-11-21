d214b968d543e68661681bc7436d8bc98c393215
James Moore <james@knowthen.com>
2023-11-21 13:44:57 -0800

    add --source --destination (#9)

--- src/config.rs
+++ src/config.rs
@@ -6,8 +6,8 @@ impl Config {
         Config {
             node: None,
             server: None,
-            sources: Vec::new(),
-            destinations: Vec::new(),
+            sources: None,
+            destinations: None,
         }
     }
     pub fn set_node(&mut self, display_name: String, unique_id: String, storage_path: String) {
@@ -25,14 +25,24 @@ impl Config {
         display_name: String,
         journal_path: String,
     ) {
-        self.sources.push(Source::sqlite_physical_replication {
-            display_name,
-            journal_path,
-        });
+        match &mut self.sources {
+            Some(sources) => sources.push(Source::sqlite_physical_replication {
+                display_name,
+                journal_path,
+            }),
+            None => {
+                self.sources = Some(vec![Source::sqlite_physical_replication {
+                    display_name,
+                    journal_path,
+                }])
+            }
+        }
     }
     pub fn add_sqlite_connector_source(&mut self, display_name: String, path: String) {
-        self.sources
-            .push(Source::sqlite_connector { display_name, path });
+        match &mut self.sources {
+            Some(sources) => sources.push(Source::sqlite_connector { display_name, path }),
+            None => self.sources = Some(vec![Source::sqlite_connector { display_name, path }]),
+        }
     }
     pub fn add_sqlite_physical_replication_destination(
         &mut self,
@@ -40,33 +50,70 @@ impl Config {
         journal_path: String,
         database_path: String,
     ) {
-        self.destinations
-            .push(Destination::sqlite_physical_replication {
+        match &mut self.destinations {
+            Some(destinations) => destinations.push(Destination::sqlite_physical_replication {
                 display_name,
                 journal_path,
                 database_path,
-            });
+            }),
+            None => {
+                self.destinations = Some(vec![Destination::sqlite_physical_replication {
+                    display_name,
+                    journal_path,
+                    database_path,
+                }])
+            }
+        }
     }
     pub fn add_sqlite_connector_destination(&mut self, display_name: String, path: String) {
-        self.destinations
-            .push(Destination::sqlite_connector { display_name, path });
+        match &mut self.destinations {
+            Some(destinations) => {
+                destinations.push(Destination::sqlite_connector { display_name, path })
+            }
+            None => {
+                self.destinations = Some(vec![Destination::sqlite_connector { display_name, path }])
+            }
+        }
     }
     pub fn add_postgres_connector_destination(&mut self, display_name: String, url: String) {
-        self.destinations
-            .push(Destination::postgres_connector { display_name, url });
+        match &mut self.destinations {
+            Some(destinations) => {
+                destinations.push(Destination::postgres_connector { display_name, url })
+            }
+            None => {
+                self.destinations =
+                    Some(vec![Destination::postgres_connector { display_name, url }])
+            }
+        }
     }
     pub fn add_mysql_connector_destination(&mut self, display_name: String, url: String) {
-        self.destinations
-            .push(Destination::mysql_connector { display_name, url });
+        match &mut self.destinations {
+            Some(destinations) => {
+                destinations.push(Destination::mysql_connector { display_name, url })
+            }
+            None => {
+                self.destinations = Some(vec![Destination::mysql_connector { display_name, url }])
+            }
+        }
     }
     pub fn add_kafka_destination(&mut self, display_name: String, brokers: String, topic: String) {
-        self.destinations.push(Destination::kafka {
-            display_name,
-            brokers,
-            topic,
-        });
+        match &mut self.destinations {
+            Some(destinations) => destinations.push(Destination::kafka {
+                display_name,
+                brokers,
+                topic,
+            }),
+            None => {
+                self.destinations = Some(vec![Destination::kafka {
+                    display_name,
+                    brokers,
+                    topic,
+                }])
+            }
+        }
     }
-    pub fn save(&self, path: String) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn save<T: AsRef<str>>(&self, path: T) -> Result<(), Box<dyn std::error::Error>> {
+        let path = path.as_ref();
         let toml = toml::to_string(&self)?;
         fs::write(path, toml)?;
         Ok(())
@@ -88,8 +135,8 @@ impl Config {
 pub struct Config {
     node: Option<Node>,
     server: Option<Server>,
-    sources: Vec<Source>,
-    destinations: Vec<Destination>,
+    sources: Option<Vec<Source>>,
+    destinations: Option<Vec<Destination>>,
 }
 
 #[derive(Serialize, Deserialize)]
